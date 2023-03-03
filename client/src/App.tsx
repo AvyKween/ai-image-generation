2482cfd52ba966d7bbaf22e021f8c23bc7d65d55
AvyKween <waldorfroque@hotmail.com>
2023-03-03 00:20:25 -0500

    Routes added to App.tsx

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,9 +1,21 @@
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
-const App = () => {
+import { Header } from './components';
+import { CreatePost, Home } from './pages';
+
+function App() {  
+
   return (
-    <h1 className="text-3x1 font-bold underline">
-      Test
-    </h1>
+    <BrowserRouter>
+      <Header />
+
+      <main className="sm:p-8 px-4 py-8 w-full bg-[#f9fafe] min-h-[calc(100vh-73px)]">
+        <Routes>
+          <Route path='/' element={ <Home/> }/>
+          <Route path='/create-post' element={ <CreatePost/> }/>
+        </Routes>
+      </main>
+    </BrowserRouter>
   )
 }
 
