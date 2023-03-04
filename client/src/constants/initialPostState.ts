2ac0c4c732c3b3e54c39be6ba282700a5a2e1c33
AvyKween <waldorfroque@hotmail.com>
2023-03-04 00:34:30 -0500

    Fixed constants declarations and exports

---
+++ client/src/constants/initialPostState.ts
@@ -0,0 +1,5 @@
+export const initialPostState = {
+    name: '',
+    prompt: '',
+    photo: '',
+  }
---
+++ client/src/constants/testData.ts
@@ -0,0 +1,8 @@
+import { Post } from "../interfaces";
+
+export const testData: Post[] = [
+    {
+      _id: '123',
+      createdAt: 10
+    }
+]
